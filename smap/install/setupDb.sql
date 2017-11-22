8d8b05cc9475eadb05b2c65205643e6cd0147797
Neil Penman <neilpenman@gmail.com>
2017-11-22 12:46:06 +0800

    Does not add instanceid to database

--- smap/install/setupDb.sql
+++ smap/install/setupDb.sql
@@ -424,7 +424,8 @@ CREATE TABLE survey (
 	shared_table boolean default false,				-- True if this survey shares its table
 	pulldata text,									-- Settings to customise pulling data from another survey into a csv file
 	exclude_empty boolean default false,				-- True if reports should not include empty data
-	created timestamp with time zone				-- Date / Time the survey was created
+	created timestamp with time zone,				-- Date / Time the survey was created
+	meta text										-- Meta items to collect with this survey
 	);
 ALTER TABLE survey OWNER TO ws;
 DROP INDEX IF EXISTS SurveyDisplayName;
