34e8fc077b9319caca48ce03ed4121a0201526b8
Neil Penman <neilpenman@gmail.com>
2017-12-21 10:41:59 +1100

    Moved chart data to calculate and made calculate accessible in online editor

--- smap/deploy/sd.sql
+++ smap/deploy/sd.sql
@@ -799,7 +799,6 @@ alter table forward add column filter text;
 -- Upgrade to 17.11
 alter table organisation add column ft_send text;
 alter table organisation add column ft_delete text;
-alter table question add column chartdata text;
 
 -- Upgrade to 17.12
 alter table server add column document_sync boolean;
