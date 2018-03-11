2c094a309b5253794d002fbe3865cd72fa9a950f
Neil Penman <neilpenman@gmail.com>
2018-03-11 15:42:24 +0100

    Made the new reports page the default page

--- smap/install/apacheConfig.sh
+++ smap/install/apacheConfig.sh
@@ -6,5 +6,16 @@ a_config_dir="/etc/apache2/sites-available"
 sudo cp $a_config_dir/smap-volatile.conf $a_config_dir/smap-volatile.conf.bu
 sudo cp config_files/a24-smap-volatile.conf $a_config_dir/smap-volatile.conf
 
+# Add a default setting of 127.0.0.1 as the DBHOST if it has not already been set
+dbhost_set=`grep DBHOST /etc/environment | wc -l`
+if [ $dbhost_set -eq 0 ]; then
+    echo "export DBHOST=127.0.0.1" >> /etc/environment
+fi
+
+# Ensure apache loads the environment variables
+avars_set=`grep "\. /etc/environment" /etc/apache2/envvars | wc -l`
+if [ $avars_set -eq 0 ]; then
+    echo ". /etc/environment" >> /etc/apache2/envvars
+fi
 sudo service apache2 reload
 
