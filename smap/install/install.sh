3231d307815f1b0b6c0e089ac4467b74a8922bb9
Neil Penman <neilpenman@gmail.com>
2018-04-03 07:14:36 +0100

    Clean up shared resource tables on delete of resource, survey, organisation

--- smap/install/install.sh
+++ smap/install/install.sh
@@ -349,7 +349,7 @@ sudo apt-get install gdal-bin -y
 sudo apt-get install ttf-dejavu -y
 
 # Add a file containing the version number
-echo "1803" > ~/smap_version
+echo "1804" > ~/smap_version
 
 echo '##### 21. Deploy Smap'
 cd ../deploy
