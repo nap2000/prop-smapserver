d15e090b8c1dab5b96abbec1d167c4cf6167a9fa
Neil Penman <neilpenman@gmail.com>
2018-07-19 17:40:14 +0200

    Added restore data functionality back

--- smap/deploy/sd.sql
+++ smap/deploy/sd.sql
@@ -995,6 +995,7 @@ alter table organisation add column email_task boolean;
 -- Speed up loading of data into results db
 alter table upload_event add column results_db_applied boolean default false;
 create index idx_ue_ident on upload_event(results_db_applied);
+update upload_event set results_db_applied = 'true' where ue_id in (select ue_id from subscriber_event where subscriber = 'results_db');
 
 
 
