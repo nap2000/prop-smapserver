8c1c0cdc0421074d5dd2347b3fd76d2efdce410f
Neil Penman <neilpenman@gmail.com>
2018-07-21 11:27:41 +0200

    Adding navigation 1 to many - not working yet

--- odata/src/smapModels/SurveyForm.java
+++ odata/src/smapModels/SurveyForm.java
@@ -7,12 +7,12 @@ import org.smap.sdal.model.TableColumn;
 public class SurveyForm {
 	public int id;
 	public String name;
-	public String referenceName;		// The name of the form that contains the data used by a reference form
 	public int parentform;
 	public int parentFormIndex;		// Used by the editor instead of the parent form id which may not be known during form creation
 	public int parentQuestion;
 	public int parentQuestionIndex;
 	public String tableName;			// Name of the table that holds the results for this form
 	public ArrayList<TableColumn> columns;
+	public ArrayList<SurveyNavigation> navigation = new ArrayList<SurveyNavigation> ();
 	public SurveyModel survey;	// Link to the survey that defines this form
 }
--- odata/src/smapModels/SurveyModel.java
+++ odata/src/smapModels/SurveyModel.java
@@ -1,5 +1,6 @@
 package smapModels;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.olingo.commons.api.edm.FullQualifiedName;
@@ -8,4 +9,5 @@ public class SurveyModel {
 	public int id;
 	public String ident;
 	public String name;
+	ArrayList<SurveyForm> forms = new ArrayList<>();
 }
