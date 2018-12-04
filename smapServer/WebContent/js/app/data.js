80c5c21249ce1af62332400620bef6f468c934f1
Neil Penman <neilpenman@gmail.com>
2018-12-04 13:24:03 +1000

    Pass tz in call to get items

--- smapServer/WebContent/js/app/data.js
+++ smapServer/WebContent/js/app/data.js
@@ -226,8 +226,11 @@ function processSurveyData(fId, f_sId, f_view, survey, replace, start_rec) {
 	
 	var survey,
 		i,
+		tz = Intl.DateTimeFormat().resolvedOptions().timeZone,      // use local time zone
 		url = formItemsURL(fId, "yes", "no", start_rec, rec_limit, bBad, f_view.filter,
-				f_view.dateQuestionId, f_view.fromDate, f_view.toDate, f_view.advanced_filter);	// Get all records with all features
+				f_view.dateQuestionId, f_view.fromDate, f_view.toDate,
+				f_view.advanced_filter,  // Get all records with all features
+				tz);
 	
 	addHourglass();
  	$.ajax({
