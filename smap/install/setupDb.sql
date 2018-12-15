4935376af6111948ac8fa04b799818387ef9ef04
Neil Penman <neilpenman@gmail.com>
2018-12-15 13:29:54 +1100

    Pass user id to get user data function

--- smap/install/setupDb.sql
+++ smap/install/setupDb.sql
@@ -730,7 +730,8 @@ CREATE TABLE dashboard_settings (
 	ds_to_date date,
 	ds_filter text,
 	ds_advanced_filter text,
-	ds_subject_type
+	ds_subject_type text,
+	ds_u_id integer
 	);
 alter table dashboard_settings add constraint ds_user_ident FOREIGN KEY (ds_user_ident)
 	REFERENCES users (ident) MATCH SIMPLE
