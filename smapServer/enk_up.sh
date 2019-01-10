9a9a2853859e06cc746fb623a8d68e3764759888
Neil Penman <neilpenman@gmail.com>
2019-01-11 07:57:13 +1000

    Updated latest versions of css from webform

--- smapServer/enk_up.sh
+++ smapServer/enk_up.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cp ~/git/webform/build/js/enketo-bundle.js ./WebContent/build/js/webform-bundle.js
-#cp ~/git/webform/build/css/* ./WebContent/build/css
+cp ~/git/webform/build/css/* ./WebContent/build/css
 cp -rf ~/git/webform/locales/* ./WebContent/build/locales
