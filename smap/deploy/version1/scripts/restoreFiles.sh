c0cc003917b1761bc81dfb6becd468cab0177998
Neil Penman <neilpenman@gmail.com>
2019-03-17 09:15:08 +1000

    Include guidance_hint in export of template.  update spanish translations, add region to restore files scipt

--- smap/deploy/version1/scripts/restoreFiles.sh
+++ smap/deploy/version1/scripts/restoreFiles.sh
@@ -10,8 +10,10 @@ echo "processing $0 $1"
 ident=$1
 restoreDir="/smap/uploadedSurveys/$ident"
 awsPath="s3://`cat ~ubuntu/bucket`/uploadedSurveys/$ident"
+region=`cat ~ubuntu/region`
 
-/usr/local/bin/aws s3 sync $awsPath $restoreDir
+echo "/usr/local/bin/aws s3 sync --region $region $awsPath $restoreDir"
+/usr/local/bin/aws s3 sync --region $region $awsPath $restoreDir
 exitValue=$?
 
 exit $exitValue
