0c9f341d29874999e1a69690b4fb0eab515daf20
Neil Penman <neilpenman@gmail.com>
2019-04-18 12:37:51 +1000

    Add restore of media files before exchange export

--- smap/deploy/version1/scripts/restoreFiles.sh
+++ smap/deploy/version1/scripts/restoreFiles.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
-if [ $# -lt "1" ]; then
-        echo "usage $0 survey_ident"
+if [ $# -lt "2" ]; then
+        echo "usage $0 survey_ident attachments | uploadedSurveys"
 fi
 
+exitValue=0
+
 echo "================================================="
-echo "processing $0 $1" 
+echo "processing $0 $1 $2" 
 
-ident=$1
-restoreDir="/smap/uploadedSurveys/$ident"
-awsPath="s3://`cat ~ubuntu/bucket`/uploadedSurveys/$ident"
-region=`cat ~ubuntu/region`
-
-echo "/usr/local/bin/aws s3 sync --region $region $awsPath $restoreDir"
-/usr/local/bin/aws s3 sync --region $region $awsPath $restoreDir
-exitValue=$?
-
+if [ -f ~ubuntu/bucket ]; then
+	ident=$1
+	type=$2
+	restoreDir="/smap/$type/$ident"
+	awsPath="s3://`cat ~ubuntu/bucket`/$type/$ident"
+	region=`cat ~ubuntu/region`
+	
+	/usr/local/bin/aws s3 sync --region $region $awsPath $restoreDir
+	exitValue=$?
+fi
+	
 exit $exitValue
+	
