9134e0f6a201302c2c59e3a4ad0e9850c997e5b7
Neil Penman <neilpenman@gmail.com>
2019-06-14 10:45:21 +1000

    Include lightening of menu items in theme

--- smapServer/WebContent/js/app/theme.js
+++ smapServer/WebContent/js/app/theme.js
@@ -21,6 +21,7 @@ along with SMAP.  If not, see <http://www.gnu.org/licenses/>.
  * Set page themes
  */
 var navbarColor = localStorage.getItem("navbar_color");
+var navbarLight = LightenDarkenColor(navbarColor, 20);
 if(navbarColor) {
     var head = document.getElementsByTagName('head')[0];
     var style = document.createElement('style');
@@ -29,12 +30,43 @@ if(navbarColor) {
     // header.navbar-default legacy WB banner
     // #header legacy jquery UI banner
     // Other elements are for current navbar
-    style.innerHTML = 'header.navbar-default, #header, body, .mini-navbar, .nav-second-level, .nav > li.active '
-        + '{ background-color: ' + navbarColor + '; background: ' + navbarColor + '};'
-        + '.navbar-default .nav > li > a:hover,.navbar-default .nav > li > a:focus, ul.nav-second-level, .canvas-menu.mini-navbar .nav-second-level, .nav > li.active '
-        + '{ background-color: #fb3d08 }';
+    style.innerHTML = 'header.navbar-default, #header, body, .mini-navbar, .nav-second-level, .nav > li.active, .navbar-default .nav > li > a:focus '
+        + '{ background-color: ' + navbarColor + '; background: ' + navbarColor + '}'
+        + ' .navbar-default .nav > li > a:hover, ul.nav-second-level, .canvas-menu.mini-navbar .nav-second-level '
+        + '{ background-color: ' + navbarLight + '; background: ' + navbarLight + '}';
 
     head.appendChild(style);
+}
+
+// From https://css-tricks.com/snippets/javascript/lighten-darken-color/
+function LightenDarkenColor(col, amt) {
+
+    var usePound = false;
+
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+
 }
 
 
