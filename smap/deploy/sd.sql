7459953694543d2bc7524cf120824d50bec10da7
Neil Penman <neilpenman@gmail.com>
2019-07-07 16:00:47 +1000

    refactor the key policies, enable a key policy of replace

--- smap/deploy/sd.sql
+++ smap/deploy/sd.sql
@@ -1206,3 +1206,8 @@ alter table notification_log add column type text;
 alter table organisation add column navbar_color text;
 update organisation set navbar_color = '#2c3c28' where navbar_color is null;
 alter table organisation add column can_sms boolean default false;
+
+-- Default key policy is now 'merge', policy of 'add' is to be replaced with 'merge' as it is no longer supported
+update survey set key_policy = 'merge' where key_policy = 'none';
+update survey set key_policy = 'merge' where key_policy = 'add';
+
