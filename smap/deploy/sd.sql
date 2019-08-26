7d2419e8bac1ab0d2a76f08f842b0ac83df933a9
Neil Penman <neilpenman@gmail.com>
2019-08-26 18:29:48 +1000

    Collect style info in a list on load

--- smap/deploy/sd.sql
+++ smap/deploy/sd.sql
@@ -4,3 +4,16 @@
 
 -- Upgrade to 19.09+
 alter table task_group add column complete_all boolean;
+
+CREATE SEQUENCE style_seq START 1;
+ALTER SEQUENCE style_seq OWNER TO ws;
+
+create TABLE style (
+	id integer default nextval('style_seq') constraint pk_style primary key,
+	s_ident text REFERENCES survey(ident) ON DELETE CASCADE,
+	name text,
+	style text	-- json
+	);
+ALTER TABLE style OWNER TO ws;
+
+alter table question add column style_id integer default 0;
