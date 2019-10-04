1616dad66eae258ccb5532af205d75f8bc357240
Neil Penman <neilpenman@gmail.com>
2019-10-04 18:33:45 +1000

    Improve error reporting in console

--- smap/deploy/sd.sql
+++ smap/deploy/sd.sql
@@ -36,3 +36,5 @@ ALTER TABLE last_refresh_log OWNER TO ws;
 alter table group_survey add column f_name text;
 
 CREATE UNIQUE INDEX record_event_key ON record_event(key);
+
+update question set source = null where qtype = 'server_calculate' and source is not null;
