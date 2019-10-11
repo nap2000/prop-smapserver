4fd190431398f4f1eeff3c8f1bfeccaf1b76dc74
Neil Penman <neilpenman@gmail.com>
2019-10-12 07:56:17 +1000

    Add training menu item

--- smap/install/setupDb.sql
+++ smap/install/setupDb.sql
@@ -178,6 +178,7 @@ create TABLE organisation (
 	sensitive_data text,			-- Questions that should be stored more securely
 	webform text,				-- Webform options
 	navbar_color text,
+	training text,
 	changed_ts TIMESTAMP WITH TIME ZONE
 	);
 CREATE UNIQUE INDEX idx_organisation ON organisation(name);
