f512fb8e614f7d00d53d2af50fdcd46c103d96d9
Neil Penman <neilpenman@gmail.com>
2020-04-08 07:16:07 +1000

    Partial addition of translate to billing.  Fails on local test possibly due to jdbc version issue

--- smap/deploy/rates.sql
+++ smap/deploy/rates.sql
@@ -9,6 +9,7 @@ values (0,
 		{"item": "1", "name": "submissions", "unitCost":"0.01", "free":"100"},
 		{"item": "2", "name": "disk", "unitCost":"0.25", "free":"20"},
 		{"item": "3", "name": "rekognition", "unitCost":"0.02", "free":"100"},
+		{"item": "4", "name": "translate", "unitCost":"0.00003", "free":"50000"},
 		{"item": "4", "name": "static_map", "unitCost":"0", "free":"0"},
 		{"item": "5", "name": "monthly", "unitCost":"50", "free":"0"}
 	]',
