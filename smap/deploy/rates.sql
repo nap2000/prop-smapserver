ea285e879d57d726d892dca3645db16498627655
Neil Penman <neilpenman@gmail.com>
2020-04-21 14:59:37 +1000

    Udpate rates

--- smap/deploy/rates.sql
+++ smap/deploy/rates.sql
@@ -7,9 +7,9 @@ values (0,
 	'[
 		{"item": "1", "name": "submissions", "unitCost":"0.01", "free":"100"},
 		{"item": "2", "name": "disk", "unitCost":"0.25", "free":"20"},
-		{"item": "3", "name": "rekognition", "unitCost":"0.025", "free":"100"},
-		{"item": "6", "name": "translate", "unitCost":"0.00003", "free":"5000"},
-		{"item": "7", "name": "transcribe", "unitCost":"0.0004", "free":"100"},
+		{"item": "3", "name": "rekognition", "unitCost":"0.0025", "free":"1000"},
+		{"item": "6", "name": "translate", "unitCost":"0.00003", "free":"50000"},
+		{"item": "7", "name": "transcribe", "unitCost":"0.0008", "free":"1000"},
 		{"item": "4", "name": "static_map", "unitCost":"0", "free":"0"},
 		{"item": "5", "name": "monthly", "unitCost":"50", "free":"0"}
 	]',
