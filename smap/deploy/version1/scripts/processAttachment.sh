bd4042817ea4117038ebbe27894388f53ae31d9b
Neil Penman <neilpenman@gmail.com>
2020-06-02 14:42:16 +1000

    Remove FLV creation

--- smap/deploy/version1/scripts/processAttachment.sh
+++ smap/deploy/version1/scripts/processAttachment.sh
@@ -13,7 +13,6 @@ contenttype=$3
 ext=$4
 destfile="$destdir/$filename.$ext"
 destthumbnail="$destdir/thumbs/$filename.$ext.jpg"
-destflv="$destdir/flv/$filename.$ext.flv"
 
 
 # If content type is "image" create a thumbnail
@@ -28,28 +27,13 @@ if [ x"$type" = ximage ]; then
 	sh -c "/usr/bin/convert -background white -alpha remove $destfile $destfile"
 fi
 
-#If content type is "video" create a thumbnail and a flowplayer friendly video
+#If content type is "video" create a thumbnail 
 if [ x"$type" = xvideo ]; then
 	echo "--------------------------------------"
 	echo "Creating thumbnails $destthumbnail from $destfile"
 	rm $destthumbnail
 	sh -c "/usr/bin/ffmpeg -i $destfile -vf scale=-1:100  $destthumbnail"
-
-	echo "--------------------------------------"
-	echo "Creating flash movie and adding meta data"
-	rm $destflv
-	sh -c "/usr/bin/ffmpeg -i $destfile -ar 22050 -f flv $destflv"
-	sh -c "flvtool2 -UP $destflv"
 fi
-
-#If content type is "audio" create a flowplayer friendly video
-#if [ x"$type" = xaudio ]; then
-#	echo "--------------------------------------"
-#	echo "Creating flash movie and adding meta data"
-#	rm $destflv
-#	sh -c "/usr/bin/ffmpeg -i $destfile -ar 22050 -f flv $destflv"
-#	sh -c "flvtool2 -UP $destflv"
-#fi
 
 # If there is an s3 bucket available then send files to it
 if [ -f ~ubuntu/bucket ]; then
@@ -67,10 +51,4 @@ if [ -f ~ubuntu/bucket ]; then
                 awsPath="s3://`cat ~ubuntu/bucket`$relPath"
                 /usr/bin/aws s3 --region $region cp $destthumbnail $awsPath
         fi
-        if [ -f  $destflv ]; then
-                relPath=${destflv#"$prefix"}
-                awsPath="s3://`cat ~ubuntu/bucket`$relPath"
-                /usr/bin/aws s3 cp --region $region $destflv $awsPath
-        fi
-
 fi
