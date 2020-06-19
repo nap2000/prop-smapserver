6e8c313e5b9fb0ffc6d1c70cee8be5a7672179cc
Neil Penman <neilpenman@gmail.com>
2020-06-19 16:37:23 +1000

    Further work on custom report

---
+++ tdh/src/model/DailyReportsConfig.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public class DailyReportsConfig {
+
+	public String sIdent;
+	public String dateName;
+	public ArrayList<ReportColumn> columns;
+	public ArrayList<ReportMultiColumn> bars;
+}
---
+++ tdh/src/model/ReportMultiColumn.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.ArrayList;
+
+public class ReportMultiColumn {
+	public String name;
+	public String title;
+	public ArrayList<String> columns;
+}
