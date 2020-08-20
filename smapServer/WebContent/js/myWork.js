4ee97165a30e62474d48987523849e07361e30b7
Neil Penman <neilpenman@gmail.com>
2020-08-20 13:34:27 +1000

    More changes to mywork

--- smapServer/WebContent/js/myWork.js
+++ smapServer/WebContent/js/myWork.js
@@ -90,6 +90,18 @@ require([
 			}
 		});
 
+		/*
+		 * Register service worker
+		 */
+		if ('serviceWorker' in navigator) {
+				navigator.serviceWorker.register('/js/workers/myWorkWorker.js').then(function(registration) {
+					// Registration was successful
+					console.log('ServiceWorker registration successful with scope: ', registration.scope);
+				}, function(err) {
+					// registration failed :(
+					console.log('ServiceWorker registration failed: ', err);
+				});
+		}
 
 	});
 
