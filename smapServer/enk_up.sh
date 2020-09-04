d21525a4996309e5cf74ca732628a978dbad56ec
Neil Penman <neilpenman@gmail.com>
2020-09-04 13:25:15 +1000

    Set my work flag on new forms

--- smapServer/enk_up.sh
+++ smapServer/enk_up.sh
@@ -4,3 +4,5 @@ cp ~/git/webform/build/js/enketo-bundle.js ./WebContent/build/js/webform-bundle.
 cp ~/git/webform/build/css/* ./WebContent/build/css
 cp ~/git/webform/build/fonts/* ./WebContent/build/fonts
 cp -rf ~/git/webform/locales/* ./WebContent/build/locales
+
+cp ~/git/webform/webform/file-storage.js ./WebContent/build/js/file-storage.js
