87cb4528be6f1c35fa90796072b91fb0e18d8409
Neil Penman <neilpenman@gmail.com>
2020-09-30 08:12:45 +1000

    Add aws credentials to java environment

--- smap/deploy/deploy.sh
+++ smap/deploy/deploy.sh
@@ -126,6 +126,11 @@ then
             cp $deploy_from/resources/properties/credentials $f
     done
 fi
+sudo cp  $deploy_from/resources/properties/setcredentials.sh /smap_bin
+envset=`cat /usr/share/$TOMCAT_VERSION/bin/setenv.sh | grep -c "setcredentials"`
+if [ $envset -eq 0 ]; then
+	echo "/smap_bin/setcredentials.sh" | sudo tee -a /usr/share/$TOMCAT_VERSION/bin/setenv.sh 
+fi
 
 cd /var/log/subscribers
 rm *.log_old
