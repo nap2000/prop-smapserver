2368f5781a9181d6a849c4823f39b63fa4ae7faf
Neil Penman <neilpenman@gmail.com>
2020-12-17 17:38:34 +1000

    Remove melody from surveyKPI

---
+++ smapServer/WebContent/js/app/unregisterServiceWorker.js
@@ -0,0 +1,12 @@
+/*
+ * This file hopefully will allow recovery of a serviceworker enabled version of the client has to be reversed out and replaced
+ * by a version without service worker
+ * In the service worker version of the app the code should be commented out
+ * Also this file should not be added to the cache
+ */
+
+navigator.serviceWorker.getRegistrations().then( function(registrations) {
+	for(let registration of registrations) {
+		registration.unregister();
+	}
+});
