71143ad1ddbe29c8f9ec6e9f225ca04b6d240628
Neil Penman <neilpenman@gmail.com>
2020-12-18 14:19:41 +1000

    Make uplaod template dialog acceptable

--- smapServer/WebContent/js/nls/root/lang.js
+++ smapServer/WebContent/js/nls/root/lang.js
@@ -386,6 +386,7 @@ define({
     "tm_add_for": "Add Forward",
     "tm_ref_f": "Refresh Form List",
     "tm_o_u": "Original Upload Page",
+    "tm_gw": "Groups are only required for surveys that share tables, if in doubt select none",
 
     // Analysis
     "a_ref_rate": "Analysis auto refresh interval",
