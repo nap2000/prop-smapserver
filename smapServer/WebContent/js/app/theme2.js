9a590832dbd9413822731a2563a9a8eb53512563
Neil Penman <neilpenman@gmail.com>
2021-01-07 22:06:53 +1000

    Add ability to set navbar text color

--- smapServer/WebContent/js/app/theme2.js
+++ smapServer/WebContent/js/app/theme2.js
@@ -22,8 +22,9 @@ along with SMAP.  If not, see <http://www.gnu.org/licenses/>.
  */
 try {
 	var navbarColor = localStorage.getItem("navbar_color");
+	var navbarTextColor = localStorage.getItem("navbar_text_color");
 	var navbarLight = LightenDarkenColor(navbarColor, 20);
-	if(navbarColor) {
+	if(navbarColor && navbarTextColor) {
 		var head = document.getElementsByTagName('head')[0];
 		var style = document.createElement('style');
 		style.setAttribute("id", "navbar_color");
@@ -32,9 +33,9 @@ try {
 		// #header legacy jquery UI banner
 		// Other elements are for current navbar
 		style.innerHTML = 'nav.navbar-smap, .bg-navbar-smap,  .navbar-smap .navbar-toggler, .navbar-smap .navbar-brand, .navbar-smap .navbar-nav .nav-link , .navbar-smap .nav > li > a:focus '
-			+ '{ background-color: ' + navbarColor + '; background: ' + navbarColor + ' !important}'
+			+ '{ background-color: ' + navbarColor + '; background: ' + navbarColor + ' !important; color: ' + navbarTextColor +'!important}'
 			+ ' nav.navbar-smap .nav > li > a:hover,, .bg-navbar-smap .nav > li > a:hover, ul.nav-second-level, .canvas-menu.mini-navbar .nav-second-level '
-			+ '{ background-color: ' + navbarLight + '; background: ' + navbarLight + ' !important}';
+			+ '{ background-color: ' + navbarLight + '; background: ' + navbarLight + ' !important; color: ' + navbarTextColor +'!important}';
 
 		head.appendChild(style);
 	}
