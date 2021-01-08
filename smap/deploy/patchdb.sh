f28564582b66d0c27a5e4659e481caffb9bd292b
Neil Penman <neilpenman@gmail.com>
2021-01-08 13:43:10 +1000

    Update version number to 21.01

--- smap/deploy/patchdb.sh
+++ smap/deploy/patchdb.sh
@@ -305,5 +305,5 @@ echo "COPY language_codes(code, aws_translate, aws_transcribe, transcribe_defaul
 
 
 # update version reference
-new_version="2010"
+new_version="2101"
 echo "$new_version" > ~/smap_version
