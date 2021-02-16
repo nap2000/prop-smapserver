34e607bf05a20e818cfa30de98ccae91091b42f4
Neil Penman <neilpenman@gmail.com>
2021-02-16 15:04:10 +1000

    Removed optimisations on webform bundle

--- smapServer/dep.sh
+++ smapServer/dep.sh
@@ -11,17 +11,19 @@
 
 #if [ "$1" != develop ]
 #then
-	# Minify webform bundle
 	rm WebContent/build/js/webform-bundle.min.js
 
-	echo "--------------------------- transpiling with babel to es5"
-	babel WebContent/build/js/webform-bundle.js --out-file WebContent/build/js/webform-bundle.es5.js
+	# Skip minification
+        cp  WebContent/build/js/webform-bundle.js WebContent/build/js/webform-bundle.min.js
 
-	echo "--------------------------- google closure compile"
-	#java -jar ~/compiler-latest/closure-compiler-v20190106.jar --js WebContent/build/js/webform-bundle.es5.js --js_output_file WebContent/build/js/webform-bundle.min.js 
+	#echo "--------------------------- transpiling with babel to es5"
+	#babel WebContent/build/js/webform-bundle.js --out-file WebContent/build/js/webform-bundle.es5.js
+
+	#echo "--------------------------- google closure compile"
+	#java -jar ~/compiler-latest/closure-compiler-v20190106.jar --language_in ECMASCRIPT_2018 --js WebContent/build/js/webform-bundle.js --js_output_file WebContent/build/js/webform-bundle.min.js 
 
 	# Use whitespace optimisation only "SIMPLE_OPTIMIZATION" as full opimisation causes code that triggers enabling of widgets to not work
-	java -jar ~/compiler-latest/closure-compiler-v20200719.jar --force_inject_library es6_runtime --compilation_level WHITESPACE_ONLY --js WebContent/build/js/webform-bundle.es5.js --js_output_file WebContent/build/js/webform-bundle.min.js 
+	java -jar ~/compiler-latest/closure-compiler-v20200719.jar --force_inject_library es6_runtime --compilation_level WHITESPACE_ONLY --js WebContent/build/js/webform-bundle.js --js_output_file WebContent/build/js/webform-bundle.min.js 
 
 	# Use full optimisation as whitespace only caluse error on submit with missing jscomp
 	#java -jar ~/compiler-latest/closure-compiler-v20200719.jar  --js WebContent/build/js/webform-bundle.es5.js --js_output_file WebContent/build/js/webform-bundle.min.js 
