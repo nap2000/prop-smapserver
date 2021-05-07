6acb3688c4c90658001abca3cec4c54d5c38a717
Neil Penman <neilpenman@gmail.com>
2021-05-07 13:37:27 +1000

    Align contact footers

--- tasks/WebContent/js/contacts.js
+++ tasks/WebContent/js/contacts.js
@@ -99,21 +99,23 @@ require([
 			initComplete: function () {
 				this.api().columns().every( function () {
 					var column = this;
-					var select = $('<select><option value=""></option></select>')
-						.appendTo( $(column.footer()).empty() )
-						.on( 'change', function () {
-							var val = $.fn.dataTable.util.escapeRegex(
-								$(this).val()
-							);
+					if (column.index() === 1 || column.index() === 2) {
+						var select = $('<select><option value=""></option></select>')
+							.appendTo($(column.footer()).empty())
+							.on('change', function () {
+								var val = $.fn.dataTable.util.escapeRegex(
+									$(this).val()
+								);
 
-							column
-								.search( val ? '^'+val+'$' : '', true, false )
-								.draw();
-						} );
+								column
+									.search(val ? '^' + val + '$' : '', true, false)
+									.draw();
+							});
 
-					column.data().unique().sort().each( function ( d, j ) {
-						select.append( '<option value="'+d+'">'+d+'</option>' )
-					} );
+						column.data().unique().sort().each(function (d, j) {
+							select.append('<option value="' + d + '">' + d + '</option>')
+						});
+					}
 				} );
 
 			}
