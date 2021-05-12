b4ebb67f0ad7145685c6d4378b7d775cb28ce74c
Neil Penman <neilpenman@gmail.com>
2021-05-12 10:30:13 +1000

    Add more notifications to the log archive

--- smap/deploy/archive.sql
+++ smap/deploy/archive.sql
@@ -4,7 +4,10 @@ WITH archived_rows AS (
     WHERE
         (event = 'view' or event = 'dashboard view' or event = 'API view' or event = 'API CSV view' or event = 'API single record view'
         	or event = 'user location view' or event = 'user acivity view'
-        	or event = 'create survey')
+        	or event = 'create survey'
+        	or event = 'notification'
+        	or event = 'error'
+        	or event = 'email')
         and log_time < now() - interval '100 days'
     RETURNING *
 )
