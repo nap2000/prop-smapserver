d1089bdba18cd2bffad8c9b4062382a38a4afdcf
Neil Penman <neilpenman@gmail.com>
2021-07-23 10:31:50 +1000

    Move additional log events to archive

--- smap/deploy/archive.sql
+++ smap/deploy/archive.sql
@@ -4,9 +4,19 @@ WITH archived_rows AS (
     WHERE
         (event = 'view' or event = 'dashboard view' or event = 'API view' or event = 'API CSV view' or event = 'API single record view'
         	or event = 'user location view' or event = 'user acivity view'
+        	or event = 'user'
+        	or event = 'submissions'
         	or event = 'create survey'
         	or event = 'notification'
         	or event = 'error'
+        	or event = 'block'
+        	or event = 'task'
+        	or event = 'email task'
+        	or event = 'create pdf'
+        	or event = 'import data'
+        	or event = 'delete'
+        	or event = 'restore'
+        	or event = 'mailout'
         	or event = 'email')
         and log_time < now() - interval '100 days'
     RETURNING *
