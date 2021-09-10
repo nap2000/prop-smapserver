874efa2d7665e34a54d12008dcb99d65df3da380
Neil Penman <neilpenman@gmail.com>
2021-09-10 10:44:41 +1000

    webforms runs

--- smapServer/enk_up.sh
+++ smapServer/enk_up.sh
@@ -3,6 +3,6 @@
 cp ~/git/webform/build/js/enketo-bundle.js ./WebContent/build/js/webform-bundle.js
 cp ~/git/webform/build/css/* ./WebContent/build/css
 cp ~/git/webform/build/fonts/* ./WebContent/build/fonts
-cp -rf ~/git/webform/locales/* ./WebContent/build/locales
+cp -R ~/git/webform/locales ./WebContent/build/locales
 
 cp ~/git/webform/webform/file-storage.js ./WebContent/build/js/file-storage.js
