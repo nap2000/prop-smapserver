329d3a67e72ccd33450c5d5e23c357861dddf8fa
Neil Penman <neilpenman@gmail.com>
2021-10-09 13:06:44 +1000

    Call background service when generating kml from locations

--- fieldAnalysis/WebContent/js/app/panels.js
+++ fieldAnalysis/WebContent/js/app/panels.js
@@ -777,7 +777,7 @@ function savePanels(newPanel) {
 		saveViews[i] = copyView(inViews[i]);
 	}
 
-	viewsString = JSON.stringify(saveViews);
+	let viewsString = JSON.stringify(saveViews);
 	addHourglass();
 	$.ajax({
 		  type: "POST",
