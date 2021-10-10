cda2779b67130a77f179bcbccf16050a9fb9f90f
Neil Penman <neilpenman@gmail.com>
2021-10-10 11:27:02 +1000

    Add generated panel to reports, remove custom reports panel

--- smap/install/setupDb.sql
+++ smap/install/setupDb.sql
@@ -657,8 +657,8 @@ CREATE TABLE custom_report (
 	p_id integer REFERENCES project(id) ON DELETE CASCADE,
 	survey_ident text REFERENCES survey(ident) ON DELETE CASCADE,
 	name text,
-	type_id integer	REFERENCES custom_report_type(id) ON DELETE CASCADE,
-	config text								-- Custom report columns as json object
+	type_id integer REFERENCES custom_report_type(id) ON DELETE CASCADE,
+	config text	
 	);
 ALTER TABLE custom_report OWNER TO ws;
 CREATE UNIQUE INDEX custom_report_name ON custom_report(p_id, name);
