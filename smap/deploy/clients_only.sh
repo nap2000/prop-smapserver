139982d0746412a0f0e5018e5a2df283a47ff36d
Neil Penman <neilpenman@gmail.com>
2021-10-27 21:38:27 +1000

    Added a client only deployment script

---
+++ smap/deploy/clients_only.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+# deploy clients only without stopping the server
+
+deploy_from=version1
+
+if [ -e $deploy_from/smapServer.tgz ]
+then
+	echo "Updating smapServer"
+	rm -rf /var/www/smap/OpenLayers
+	rm -rf /var/www/smap/js
+	rm -rf /var/www/smap/css
+	rm -rf /var/www/smap/*.html
+	rm -rf /var/www/smap/*.js
+        rm -rf /var/www/smap/*.json
+	tar -xzf $deploy_from/smapServer.tgz -C /var/www/smap
+fi
+
+if [ -e $deploy_from/fieldAnalysis.tgz ]
+then
+        echo "Updating fieldAnalysis"
+        rm -rf /var/www/smap/fieldAnalysis
+        rm -rf /var/www/smap/app/fieldAnalysis
+        tar -xzf $deploy_from/fieldAnalysis.tgz -C /var/www/smap/app
+fi
+
+if [ -e $deploy_from/fieldManager.tgz ]
+then
+        echo "Updating fieldManager"
+        rm -rf /var/www/smap/fieldManager
+        rm -rf /var/www/smap/app/fieldManager
+
+        tar -xzf $deploy_from/fieldManager.tgz -C /var/www/smap/app
+fi
+
+if [ -e $deploy_from/tasks.tgz ]
+then
+        echo "Updating tasks"
+        rm -rf /var/www/smap/tasks
+        rm -rf /var/www/smap/app/tasks
+        tar -xzf $deploy_from/tasks.tgz -C /var/www/smap/app
+fi
+
+if [ -e $deploy_from/myWork.tgz ]
+then
+        echo "Updating myWork"
+        rm -rf /var/www/smap/myWork
+        rm -rf /var/www/smap/app/myWork
+        tar -xzf $deploy_from/myWork.tgz -C /var/www/smap/app
+fi
+
+if [ -e $deploy_from/dashboard.tgz ]
+then
+        echo "Dashboard"
+        rm -rf /var/www/smap/dashboard
+        tar -xzf $deploy_from/dashboard.tgz -C /var/www/smap
+fi
+
+
