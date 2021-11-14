1d3f29931a747534eae92509dfaf88765774e744
Neil Penman <neilpenman@gmail.com>
2021-11-14 15:00:08 +1000

    Add misc files to deploy

--- smap/dep.sh
+++ smap/dep.sh
@@ -9,3 +9,10 @@ cp -rf ~/git/prop-smapserver/smap ~/deploy
 # Get files from open source release
 cp ~/git/smapserver2/setup/patch/*.sql ~/deploy/smap/deploy
 cp ~/git/smapserver2/setup/install/*.sql ~/deploy/smap/install
+
+# Get miscelaneous files
+cp ~/deploy/fieldTask.apk ~/deploy/smap/deploy/version1
+cp ~/deploy/fieldTaskPreJellyBean.apk ~/deploy/smap/deploy/version1
+cp ~/deploy/smapUploader.jar ~/deploy/smap/deploy/version1
+cp ~/deploy/codebook.jar ~/deploy/smap/deploy/version1
+cp -rf ~/deploy/subscribers ~/deploy/smap/deploy/version1
--- smap/deploy/deploy.sh
+++ smap/deploy/deploy.sh
@@ -129,7 +129,7 @@ cp $deploy_from/fieldTask.apk /var/www/smap
 cp $deploy_from/fieldTaskPreJellyBean.apk /var/www/smap
 cp $deploy_from/smapUploader.jar /var/www/smap
 cp $deploy_from/fieldTask.apk /var/www/default
-cp -r $deploy_from/smapIcons/WebContent/* /var/www/smap/smapIcons
+#cp -r $deploy_from/smapIcons/WebContent/* /var/www/smap/smapIcons
 cp $deploy_from/*.war /var/lib/$TOMCAT_VERSION/webapps
 chown -R $TOMCAT_USER /var/lib/$TOMCAT_VERSION/webapps
 
