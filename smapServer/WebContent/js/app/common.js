206d9ada7817ed0f036511aecff37fa97b9ccafd
Neil Penman <neilpenman@gmail.com>
2022-10-10 21:51:41 +1100

    Remove commented out code

--- smapServer/WebContent/js/app/common.js
+++ smapServer/WebContent/js/app/common.js
@@ -3716,22 +3716,6 @@ function validateEmails(emails) {
 }
 
 /*
- * Prevent the menu bar from extending over two lines
- */
-// From: http://stackoverflow.com/questions/20247945/bootstrap-3-navbar-dynamic-collapse
-/*
-function autocollapse() {
-	var $navbar = $('#autocollapse');
-    $navbar.removeClass('collapsed'); 
-    if($navbar.innerHeight() > 60) // check if we've got 2 lines
-        $navbar.addClass('collapsed'); // force collapse mode
-}
-
-$(document).on('ready', autocollapse);
-$(window).on('resize', autocollapse);
-*/
-
-/*
  * Get the roles for a survey
  */
 function getSurveyRoles(sId, selectedRoles, setall) {
