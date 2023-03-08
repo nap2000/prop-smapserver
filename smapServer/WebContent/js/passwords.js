d1406cf74e70b4ac40f1e7428374fb59b6800409
Neil Penman <neilpenman@gmail.com>
2023-03-08 12:37:49 +1000

    Updated indentation

--- smapServer/WebContent/js/passwords.js
+++ smapServer/WebContent/js/passwords.js
@@ -19,142 +19,139 @@ along with SMAP.  If not, see <http://www.gnu.org/licenses/>.
 var gUserLocale = navigator.language;
 if (Modernizr.localstorage) {
 	gUserLocale = localStorage.getItem('user_locale') || navigator.language;
-} 
+}
 
 "use strict";
 requirejs.config({
-    baseUrl: '/js/libs',
-    waitSeconds: 0,
-    locale: gUserLocale,
-    paths: {
-    	app: '/js/app',
-       	lang_location: '/js'
-    },
-    shim: {
-    	'app/common': ['jquery']
-    }
+	baseUrl: '/js/libs',
+	waitSeconds: 0,
+	locale: gUserLocale,
+	paths: {
+		app: '/js/app',
+		lang_location: '/js'
+	},
+	shim: {
+		'app/common': ['jquery']
+	}
 });
 
 require([
-         'jquery',
-         'bootstrapValidator.min',
-         'app/localise',
-         'app/common'
-         ], function($, bv, localise) {
+	'jquery',
+	'bootstrapValidator.min',
+	'app/localise',
+	'app/common'
+], function($, bv, localise) {
 
 	var gToken;
-	
-$(document).ready(function() {
-	
-	var i,
-		params,
-		pArray = [],
-		param = [];
 
-	setCustomUserForgottonPasswords();			// Apply custom javascript
-	localise.setlang();
-	$('#forgottenPasswordEmail').attr('data-bv-emailaddress-message', localise.set["msg_inv_email"]);
-	
-	// Add the organisation to the title
-	if(window.location.hostname.indexOf("smap") > 0) {
-		$('#website_id').text("Smap");
-	}
-	
-	// Get the authentication token if it has been passed in parameters
-	params = location.search.substr(location.search.indexOf("?") + 1)
-	pArray = params.split("&");
-	for (i = 0; i < pArray.length; i++) {
-		param = pArray[i].split("=");
-		if ( param[0] === "token" ) {
-			gToken = param[1];
+	$(document).ready(function() {
+
+		var i,
+			params,
+			pArray = [],
+			param = [];
+
+		setCustomUserForgottonPasswords();			// Apply custom javascript
+		localise.setlang();
+		$('#forgottenPasswordEmail').attr('data-bv-emailaddress-message', localise.set["msg_inv_email"]);
+
+		// Add the organisation to the title
+		if(window.location.hostname.indexOf("smap") > 0) {
+			$('#website_id').text("Smap");
 		}
-	}
 
-	$('#forgottenPasswordEmail, #passwordValue').change(function(){
-		$('.pwd_alert, .pwd_home').hide();
+		// Get the authentication token if it has been passed in parameters
+		params = location.search.substr(location.search.indexOf("?") + 1)
+		pArray = params.split("&");
+		for (i = 0; i < pArray.length; i++) {
+			param = pArray[i].split("=");
+			if ( param[0] === "token" ) {
+				gToken = param[1];
+			}
+		}
+
+		$('#forgottenPasswordEmail, #passwordValue').change(function(){
+			$('.pwd_alert, .pwd_home').hide();
+		});
+
+		$('#resetPassword').bootstrapValidator({
+			fields: {
+				password: {
+					validators: {
+						identical: {
+							field: 'confirmPassword',
+							message: 'The password and its confirm are not the same'
+						}
+					}
+				},
+				confirmPassword: {
+					validators: {
+						identical: {
+							field: 'password',
+							message: 'The password and its confirm are not the same'
+						}
+					}
+				}
+			}
+		});
+
+		$('#resetPasswordSubmit').click(function(e){
+			e.preventDefault();
+
+			var pd = {
+					onetime: gToken,
+					password: $('#passwordValue').val()
+				},
+				pdString;
+
+			pdString = JSON.stringify(pd);
+
+
+			addHourglass();
+			$.ajax({
+				type: "POST",
+				cache: false,
+				url: "/surveyKPI/onetimelogon?lang=" + gUserLocale,
+				data: { passwordDetails: pdString },
+				success: function(data, status) {
+					removeHourglass();
+					$('.pwd_alert').show().removeClass('alert-danger').addClass('alert-success').html(localise.set["msg_pr"]);
+					$('.pwd_home').show();
+				}, error: function(data, status) {
+					removeHourglass();
+					$('.pwd_alert').show().addClass('alert-danger').removeClass('alert-success').html(localise.set["c_error"] + ": " + data.responseText);
+				}
+			});
+		});
+
+		$('#forgottenPasswordSubmit').click(function(e){
+			e.preventDefault();
+
+			var email = $('#forgottenPasswordEmail').val();
+			$('.pwd_alert, .pwd_home').hide();
+			addHourglass();
+			$.ajax({
+				type: "GET",
+				cache: false,
+				url: "/surveyKPI/onetimelogon/" + email,
+				success: function(data, status) {
+					removeHourglass();
+					$('.pwd_alert').show().removeClass('alert-danger').addClass('alert-success').html(localise.set["msg_es"] + " " + email);
+					$('.pwd_home').show();
+				}, error: function(data, status) {
+					removeHourglass();
+					var msg = data.responseText;
+					var idx1 = msg.indexOf('ApplicationException:');
+					var idx2 = msg.indexOf('<', idx1);
+					if(idx1 > 0 && idx2 > idx1) {
+						msg = msg.substring(idx1, idx2);
+					}
+					$('.pwd_alert').show().addClass('alert-danger').removeClass('alert-success').html(localise.set["c_error"] + ": " + msg);
+				}
+			});
+		});
+
 	});
-    
-    $('#resetPassword').bootstrapValidator({
-        fields: {
-            password: {
-                validators: {
-                    identical: {
-                        field: 'confirmPassword',
-                        message: 'The password and its confirm are not the same'
-                    }
-                }
-            },
-            confirmPassword: {
-                validators: {
-                    identical: {
-                        field: 'password',
-                        message: 'The password and its confirm are not the same'
-                    }
-                }
-            }
-        }
-    });
-    
-    $('#resetPasswordSubmit').click(function(e){
-    	e.preventDefault();
-    	
-    	var pd = {
-    			onetime: gToken,
-    			password: $('#passwordValue').val()
-    		},
-    		pdString;
-    	
-    	pdString = JSON.stringify(pd);
-    	
-    	
-		addHourglass();
-    	$.ajax({
-			  type: "POST",
-			  cache: false,
-			  url: "/surveyKPI/onetimelogon?lang=" + gUserLocale,
-			  data: { passwordDetails: pdString },
-			  success: function(data, status) {
-				  removeHourglass();
-				  $('.pwd_alert').show().removeClass('alert-danger').addClass('alert-success').html(localise.set["msg_pr"]);
-				  $('.pwd_home').show();
-			  }, error: function(data, status) {
-				  removeHourglass();
-			    $('.pwd_alert').show().addClass('alert-danger').removeClass('alert-success').html(localise.set["c_error"] + ": " + data.responseText);
-			  }
-		});
-    });
-    
-    $('#forgottenPasswordSubmit').click(function(e){
-    	e.preventDefault();
-    	
-    	var email = $('#forgottenPasswordEmail').val();
-	    $('.pwd_alert, .pwd_home').hide();
-		addHourglass();
-    	$.ajax({
-			  type: "GET",
-			  cache: false,
-			  url: "/surveyKPI/onetimelogon/" + email,
-			  success: function(data, status) {
-				  removeHourglass();
-				  $('.pwd_alert').show().removeClass('alert-danger').addClass('alert-success').html(localise.set["msg_es"] + " " + email);
-				  $('.pwd_home').show();
-			  }, error: function(data, status) {
-				  removeHourglass();
-				  var msg = data.responseText;
-				  var idx1 = msg.indexOf('ApplicationException:');
-				  var idx2 = msg.indexOf('<', idx1);
-				  if(idx1 > 0 && idx2 > idx1) {
-				  	msg = msg.substring(idx1, idx2);
-				  }
-			    $('.pwd_alert').show().addClass('alert-danger').removeClass('alert-success').html(localise.set["c_error"] + ": " + msg);
-			  }
-		});
-	
-    });
-});
-
-
-
 
 });
 
