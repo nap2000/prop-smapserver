83efd722ba1cdb0fe54b48046d588aaa9b3fbca9
Neil Penman <neilpenman@gmail.com>
2024-11-27 07:49:40 +1000

    Fix issue with resubmitting save after login when updating server calculations

--- smapServer/WebContent/js/app/changeset.js
+++ smapServer/WebContent/js/app/changeset.js
@@ -152,10 +152,10 @@ define([
                         for(j = 0; j < changesInSeq[i].items.length; j++) {
                             item = changesInSeq[i].items[j];
                             if(item.property && item.property.prop == 'server_calculation') {
-                                if(item.property.oldVal) {
+                                if(item.property.oldVal && item.property.oldVal.expression) {
                                     item.property.oldVal = JSON.stringify(item.property.oldVal);
                                 }
-                                if(item.property.newVal) {
+                                if(item.property.newVal  && item.property.newVal.expression) {
                                     item.property.newVal = JSON.stringify(item.property.newVal);
                                 }
                             }
