b7dd0e583eba3b8572b9a57c619c01d332ce04ba
Neil Penman <neilpenman@gmail.com>
2025-11-04 09:13:09 +1000

    Updated dashboard deployment script

--- dashboard/dep.sh
+++ dashboard/dep.sh
@@ -4,11 +4,22 @@
 mkdir dashboard
 cp -R WebContent/* dashboard
 tar -zcf dashboard.tgz dashboard
+
+echo "----------------- dashboard"
+echo "Placing tar file in ~/deploy"
+
 mv dashboard.tgz ~/deploy
 
 # copy files to the local server
-rm -rf /Library/WebServer/Documents/dashboard
-cp -R dashboard /Library/WebServer/Documents
+
+docdir=$WEBSITE_DOCS/app/dashboard
+
+echo "Website: $WEBSITE_DOCS"
+echo "Deploying to: $docdir"
+
+rm -rf $docdir
+sudo mkdir $docdir
+cp -R dashboard $docdir
 
 rm -rf dashboard
 
